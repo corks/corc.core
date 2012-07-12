e564d1955d838606968ae79a678f124332e7dfa9
Alyx <alyx@malkier.net>
2012-07-11 18:21:45 -0700

    more work done. broken, but more work done!

--- src/str.c
+++ src/str.c
@@ -69,8 +69,8 @@ CorcString *corcstr_create(void)
     CorcString *s;
 
     s = malloc(sizeof(CorcString));
-    s->size = corcSTR_DEFAULT_NEW;
-    s->string = malloc(corcSTR_DEFAULT_NEW);
+    s->size = CORCSTR_DEFAULT_NEW;
+    s->string = malloc(CORCSTR_DEFAULT_NEW);
     *s->string = '\0';
     s->end = s->string;
     s->len = 0;
@@ -92,10 +92,7 @@ size_t corcstr_append(CorcString *cs, const char *src, size_t len)
     total = cs->len + rlen;
 
     if (total > cs->size)
-    {
-        cs->string = realloc(cs->string, total + 1);
-        cs->size = total;
-    }
+        corcstr_resize(cs, total + 1);
 
     p = cs->end;
     s = src;
@@ -110,4 +107,37 @@ size_t corcstr_append(CorcString *cs, const char *src, size_t len)
     cs->len = cs->len + rlen;
 
     return cs->len;
+}
+
+size_t corcstr_remove(CorcString *cs, size_t len)
+{
+    int i;
+    char *start;
+
+    i = 0;
+    *start = *cs->end - len;
+
+    while (*start++)
+    {
+        *start = '\0';
+        i++;
+    }
+    cs->len = cs->len - i;
+
+    return cs->len;
+}
+
+CorcString *corkstr_join(char **words, char sep, size_t count)
+{
+    int i;
+    CorcString *cs;
+    cs = corcstr_create();
+
+    for (i = 0; i < count; ++i)
+    {
+        corcstr_append(cs, words[i], sizeof(words[i]));
+        corcstr_append_char(cs, sep);
+    }
+
+    return cs;
 }
--- src/str.h
+++ src/str.h
@@ -1,8 +1,8 @@
 #include <string.h>
 
-typedef struct CorcString CorcString;
+typedef struct CorcString_ CorcString;
 
-struct CorcString
+struct CorcString_
 {
     char *string;
     char *end;
@@ -10,8 +10,32 @@ struct CorcString
     size_t size;
 };
 
-size_t corcstr_concat_raw(char *dst, const char *src, size_t siz);
-size_t corcstr_copy_raw(char *dst, const char *src, size_t siz);
+extern size_t corcstr_concat_raw(char *dst, const char *src, size_t siz);
+extern size_t corcstr_copy_raw(char *dst, const char *src, size_t siz);
 
-CorcString *corkstr_create(void);
-size_t corcstr_append(CorcString *cs, const char *src, size_t len);
+extern CorcString *corkstr_create(void);
+extern size_t corcstr_append(CorcString *cs, const char *src, size_t len);
+extern size_t corcstr_remove(CorcString *cs, size_t len);
+
+extern CorcString *corkstr_join(char **words, char sep, size_t count);
+
+static inline size_t corcstr_resize(CorcString *cs, size_t siz)
+{
+    cs->string = realloc(cs->string, siz);
+    cs->size = siz;
+}
+
+static inline size_t corcstr_append_char(CorcString *cs, char c)
+{
+    if ((cs->len + 1) == cs->size)
+        corcstr_resize(cs, cs->size + 1);
+    if (c == '\0')
+    {
+        *(cs->end + 1) = c;
+        return cs->len;
+    }
+
+    *(++cs->end) = c;
+    return ++cs->len;
+}
+
