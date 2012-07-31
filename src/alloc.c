34271cb0672e486368440048c6f860bb43757df6
Michael Rodriguez <adeo2232@gmail.com>
2012-07-31 09:39:26 -0400

    Do a *proper* malloc() wrapper. The point of this wrapper is to allow the caller to handle memory management problems that arise in the library, instead of taking arbitrary actions (printf(), exit(), etc), which in turn allows greater flexibility and control over the flow of the program using this library.

--- src/alloc.c
+++ src/alloc.c
@@ -2,17 +2,9 @@
 #include <stdlib.h>
 #include "alloc.h"
 
+static void (*alloc_cb)(void);
 
-
-static void bad_callback(void)
-{
-    fprintf(stderr, "Memory allocation failed. Exiting to avoid more things breaking...\n");
-    exit(1);
-}
-
-static corc_bad_cb alloc_cb = bad_callback;
- 
-void corc_alloc_bad_cb(corc_bad_cb cb)
+void corc_alloc_bad_cb(void (*cb)(void))
 {
     alloc_cb = cb;
 }
@@ -21,12 +13,9 @@ void *corc_alloc(size_t siz)
 {
     void *p;
     p = malloc(siz);
+
     if (p == NULL)
         alloc_cb();
-    return p;
-}
 
-void corc_free(void *ptr)
-{
-    free(ptr);
+    return p;
 }
--- src/alloc.h
+++ src/alloc.h
@@ -1,9 +1,6 @@
 #ifndef CORC_CORE_ALLOC_H
 #define CORC_CORE_ALLOC_H
 
-typedef void (*corc_bad_cb)(void);
-
 void *corc_alloc(size_t siz);
-void corc_free(void *ptr);
 
 #endif /* CORC_CORE_ALLOC_H */
